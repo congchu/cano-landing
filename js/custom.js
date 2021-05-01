ecfc41507e80012f007213294885512beffc2c0e
congchu <cookie00421@gmail.com>
2021-05-01 14:52:24 +0900

    👩🏻‍💻Fixed: Removed SendEmail Logic

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,3 @@
-
-function sendEmail (){
-    alert("hello");
-    const email = document.getElementsByName("EMAIL");
-    
-    alert(email.value);
-
-    let user_email = {
-        records: [
-          {
-            fields: { email: input_mail},
-          },
-        ],
-      };
-      
-      let response = await fetch(
-        "https://api.airtable.com/v0/apprdRViOmvRUpg5E/LandingEmails",
-        {
-          method: "POST",
-          headers: {
-            Authorization: "Bearer keywlgmSjQ2ToUwTz",
-            "Content-Type": "application/json;charset=utf-8",
-          },
-          body: JSON.stringify(user_email),
-        }
-      );
-      
-      let result = await response.json();
-      alert(result.message);
-      
+function sendEmail() {
+  //TODO : 유저 이메일 저장 로직
 }
